403d76feb5640ea50f0de988a9ca8416b055c540
David Sloan <delus.dlsloan@gmail.com>
2021-09-02 17:56:46 +0000

    Added pre-commit hook and master ut.py to run all unittests for pre-commit hook

---
+++ ut.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import unittest
+import ut_rre
+
+def test(ut):
+    suite = unittest.TestLoader().loadTestsFromModule(ut)
+    result = unittest.TextTestRunner(failfast=True).run(suite)
+    if len(result.errors) or len(result.failures) or len(result.unexpectedDuccesses):
+        exit(1)
+
+if __name__ == '__main__':
+    test(ut_rre)
