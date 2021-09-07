337e5cbfd43aa1a90cb400126dfd282c03f676a0
David Sloan <delus.dlsloan@gmail.com>
2021-09-07 22:25:33 +0000

    renamed rre to nfa in nfa tests

--- ut_nfa.py
+++ ut_nfa.py
@@ -41,7 +41,7 @@ class TestRREAST(unittest.TestCase):
         self.assertEqual(err.partial.ch, 0)
         self.assertEqual(err.partial.line, 0)
 
-    def test_rre_char_class(self):
+    def test_nfa_char_class(self):
         parser = rre.parse(b'[ab]').to_nfa()
         match_a = parser.parse(b'a')
         match_b = parser.parse(b'b')
@@ -50,7 +50,7 @@ class TestRREAST(unittest.TestCase):
         self.assertEqual(match_a.text, b'a')
         self.assertEqual(match_b.text, b'b')
 
-    def test_rre_sequence(self):
+    def test_nfa_sequence(self):
         parser = rre.parse(b'[Tt]est').to_nfa()
         match_a = parser.parse(b'test')
         match_b = parser.parse(b'Test')
@@ -59,7 +59,7 @@ class TestRREAST(unittest.TestCase):
         self.assertEqual(match_a.text, b'test')
         self.assertEqual(match_b.text, b'Test')
 
-    def test_rre_either(self):
+    def test_nfa_either(self):
         parser = rre.parse(b'abc|def').to_nfa()
         match_a = parser.parse(b'abc')
         match_b = parser.parse(b'def')
