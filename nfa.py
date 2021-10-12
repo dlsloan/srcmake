8c80ab9dfca27e9a38eabc1de01f3ba1c1332669
David Sloan <delus.dlsloan@gmail.com>
2021-10-12 22:30:06 +0000

    temp before new try

--- nfa.py
+++ nfa.py
@@ -1,3 +1,160 @@
 
-class nfa:
-    pass
+from singletonrefs import SingletonRefs
+from collections import namedtuple
+
+nfaRef = namedtuple('nfaRef', ['ref'])
+nfaRef.clone = lambda nr: nr
+
+
+class MetaNFA(type):
+    @property
+    def final(cls):
+        return cls._final
+
+
+
+class nfa(object, metaclass=MetaNFA):
+    def __init__(self, condition=None, *, _volitile=False):
+        self._condition = condition
+        self._links = tuple()
+        self._i_volatile = _volitile
+
+
+    @property
+    def _volatile(self):
+        return self._i_volatile
+    @_volatile.setter
+    def _volatile(self, value):
+        assert self._i_volatile
+        assert value == False
+        self._i_volatile = False
+
+
+    @property
+    def condition(self):
+        return self._condition
+    @condition.setter
+    def condition(self, value):
+        assert self._i_volatile 
+        self._condition = value
+
+
+    @property
+    def links(self):
+        return self._links
+    @links.setter
+    def links(self, value):
+        assert self._volatile
+        return self._links
+
+
+    @property
+    def final(self):
+        return self == nfa.final
+
+
+    def test_condition(self, s):
+        # Final is terminal and sould never be tested
+        assert self != nfa.final
+
+        if self.condition is None:
+            retval = ('', s)
+        else:
+            retval = self.condition(s)
+
+        if __debug__ and retval[0] is None:
+            assert retval[1] == s
+        elif __debug__:
+            assert retval[0] + retval[1] == s
+
+        return retval
+
+
+    def add_links(self, *links):
+        with SingletonRefs(f"{__file__}:nfs.modify") as refs:
+            return self._add_links(refs, *links)
+
+
+    def _add_links(self, refs, *links):
+        if __debug__ and 'debug_modify_op' in refs:
+            for ln in links:
+                assert ln not in refs.values()
+        other = self.clone()
+        other._links = tuple(self._clone_ln_gen(refs, other, *links))
+        return other
+
+
+    def clone(self):
+        with SingletonRefs(f"{__file__}:nfs.modify") as refs:
+            return self._clone(refs)
+
+
+    def _clone_ln(self, ln, refs, other):
+        if isinstance(ln, nfaRef):
+            if 'linkRefObjs' not in refs:
+                refs['linkRefObjs'] = set()
+            refs['linkRefObjs'].add(other)
+            has_self = self in ln.ref
+        else:
+            has_self = self in ln
+        return ln.clone() if has_self else ln
+
+    def _clone_ln_gen(self, refs, other, *extend):
+        for ln in self.links:
+            yield self._clone_ln(ln, refs, other)
+        for ln in extend:
+            yield self._clone_ln(ln, refs, other)
+
+
+    def _clone_ln_complete_gen(self, refs):
+        for ln in self._links:
+            if isinstance(ln, nfaRef):
+                if id(ln.ref) in refs:
+                    yield nfaRef(refs[id(ln.ref)])
+                    continue
+            yield ln
+
+    
+    def _clone_complete(self, refs):
+        if 'linkRefObjs' in refs:
+            for other in refs['linkRefObjs']:
+                other._links = tuple(other._clone_ln_complete_gen(refs))
+
+
+    def _clone(self, refs):
+        refs.set_on_complete(f"{__file__}:nfa.clone_complete", self._clone_complete)
+        if id(self) in refs:
+            return refs[id(self)]
+        other = nfa(condition=self._condition)
+        refs[id(self)] = other
+        other._links = tuple(self._clone_ln_gen(refs, other))
+        return other
+
+
+    def modify(self):
+        with SingletonRefs(f"{__file__}:nfs.modify") as refs:
+            if __debug__:
+                refs['debug_modify_op'] = True
+            mod = self._clone(refs)
+            return refs.extend_scope(mod)
+
+    def __repr__(self):
+        return f"nfa({id(self)})"
+
+    def __contains__(self, node):
+        if id(node) == id(self):
+            return True
+        with SingletonRefs(f"{__file__}:nfs.check") as refs:
+            if id(self) in refs:
+                return False
+            refs[id(self)] = False
+            for ln in self.links:
+                if isinstance(ln, nfaRef):
+                    if node in ln.ref:
+                        return True
+                else:
+                    if node in ln:
+                        return True
+            return False
+
+nfa._final = nfa()
--- ut_nfa.py
+++ ut_nfa.py
@@ -1,12 +1,127 @@
 #!/usr/bin/env python3
-from re import match
-from typing import Match
 import unittest
-import rre
 import nfa
 
 class TestNFA(unittest.TestCase):
-    pass
+    def testNFAInit(self):
+        n = nfa.nfa()
+        self.assertIsNone(n.condition)
+        self.assertFalse(n.final)
+        self.assertTrue(nfa.nfa.final.final)
+        self.assertEqual(len(n.links), 0)
+
+    def testNFANoContition(self):
+        n = nfa.nfa()
+        self.assertEqual(n.test_condition('123'), ('', '123'))
+
+    def testBasicNFACondition(self):
+        n = nfa.nfa(condition=lambda s : ('a', s[1:]) if s.startswith('a') else(None, s))
+        self.assertEqual(n.test_condition(''), (None, ''))
+        self.assertEqual(n.test_condition('a'), ('a', ''))
+        self.assertEqual(n.test_condition('b'), (None, 'b'))
+
+    def testBasicClone(self):
+        n = nfa.nfa(condition=lambda s : ('a', s[1:]) if s.startswith('a') else(None, s))
+        n2 = n.clone()
+        self.assertNotEqual(id(n), id(n2))
+        self.assertEqual(n.test_condition(''), n2.test_condition(''))
+        self.assertEqual(n.test_condition('a'), n2.test_condition('a'))
+        self.assertEqual(n.test_condition('b'), n2.test_condition('b'))
+
+    def testBadCondition(self):
+        n = nfa.nfa(condition=lambda s : ('a', s))
+        if __debug__:
+            with self.assertRaises(AssertionError):
+                n.test_condition('')
+
+    def testFinalError(self):
+        if __debug__:
+            with self.assertRaises(AssertionError):
+                nfa.nfa.final.test_condition('')
+            with self.assertRaises(AssertionError):
+                nfa.nfa.final.test_condition('a')
+
+    def testMakeLinked(self):
+        n = nfa.nfa()
+        n2 = n.add_links(nfa.nfa())
+        n3 = n.add_links(nfa.nfa(), nfa.nfa())
+        self.assertEqual(len(n.links), 0)
+        self.assertEqual(len(n2.links), 1)
+        self.assertTrue(isinstance(n2.links[0], nfa.nfa))
+        self.assertEqual(len(n3.links), 2)
+        self.assertTrue(isinstance(n3.links[0], nfa.nfa))
+        self.assertTrue(isinstance(n3.links[1], nfa.nfa))
+
+    def testCircularLink(self):
+        n = nfa.nfa()
+        n2 = n.add_links(n)
+        self.assertEqual(len(n.links), 0)
+        self.assertEqual(len(n2.links), 1)
+        self.assertEqual(id(n2), id(n2.links[0]))
+
+    def testCloneLinks(self):
+        n = nfa.nfa()
+        n = n.add_links(nfa.nfa())
+        n2 = n.clone()
+        self.assertNotEqual(id(n), id(n2))
+        self.assertEqual(len(n.links), len(n2.links))
+        self.assertEqual(len(n.links[0].links), len(n2.links[0].links))
+
+    def testCloneCircularLinks(self):
+        n = nfa.nfa()
+        n = n.add_links(n)
+        n2 = n.clone()
+        self.assertNotEqual(id(n), id(n2))
+        self.assertEqual(len(n.links), len(n2.links))
+        self.assertEqual(id(n2), id(n2.links[0]))
+
+    def testDeepCircularLinks(self):
+        n = nfa.nfa()
+        n = n.add_links(nfa.nfa())
+        ln_orig = n.links[0]
+        with n.modify() as n2:
+            n.links[0].add_links(n)
+        self.assertEqual(len(n.links), 1)
+        self.assertEqual(len(n.links[0].links), 0)
+        self.assertEqual(id(n.links[0]), id(ln_orig))
+        self.assertEqual(len(n2.links[0].links), 1)
+        self.assertEqual(id(n2), id(n2.links[0].links[0]))
+        self.assertNotEqual(id(n2), id(n2.links[0]))
+
+    def testBadModifyLinks(self):
+        n = nfa.nfa()
+        if __debug__:
+            with self.assertRaises(AssertionError):
+                with n.modify() as n2:
+                    n.add_links(n2)
+
+    def testRefLink(self):
+        n = nfa.nfa()
+        nr = nfa.nfa()
+        n2 = n.add_links(nfa.nfaRef(nr))
+        self.assertNotEqual(id(n), id(n2))
+        self.assertEqual(id(n2.links[0].ref), id(nr))
+        nr2 = nr.add_links(n)
+        self.assertNotEqual(id(n2.links[0].ref), id(nr2))
+
+    def testRefClone(self):
+        n = nfa.nfa()
+        n2 = nfa.nfa()
+        n = n.add_links(n2)
+        n2 = n.links[0]
+        ext_n = nfa.nfa()
+        n3 = n.add_links(nfa.nfaRef(n), nfa.nfaRef(n2), nfa.nfaRef(ext_n))
+        self.assertNotEqual(id(n), id(n3.links[1].ref))
+        self.assertNotEqual(id(n2), id(n3.links[2].ref))
+        self.assertEqual(id(n3), id(n3.links[1].ref))
+        self.assertEqual(id(n3.links[0]), id(n3.links[2].ref))
+        self.assertEqual(id(ext_n), id(n3.links[3].ref))
+
+    def testNoCopy(self):
+        n = nfa.nfa()
+        n2 = nfa.nfa()
+        n = n.add_links(n2)
+        self.assertEqual(id(n.links[0]), id(n2))
 
 
 if __name__ == '__main__':
