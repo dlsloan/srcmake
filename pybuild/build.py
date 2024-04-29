db40c645c2ea5866c6ba899181592349ff4cd576
David Sloan <delus.dlsloan@gmail.com>
2024-04-28 22:48:46 -0600

    Add clean function

--- pybuild/build.py
+++ pybuild/build.py
@@ -8,9 +8,12 @@ from pathlib import Path
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('target')
+    parser.add_argument('--clean', action='store_true')
     args = parser.parse_args()
     targ = Path(args.target)
-    if targ.suffix.lower() == '.cpp':
+    if targ.suffix == '':
+        pass # short hand for easy tab conpletion on rebuilds
+    elif targ.suffix.lower() == '.cpp':
         assert not Path(targ.parent / f"{targ.stem}.c").exists()
         targ = targ.parent / targ.stem
     elif targ.suffix.lower() == '.c':
@@ -19,14 +22,23 @@ if __name__ == '__main__':
     else:
         print(f"Error: expected .c or .cpp file not {targ.suffix}:", targ)
     env = buildfile.BuildEnv()
-    try:
-        env.build(targ).value()
-    except sp.CalledProcessError as err:
-        print(*[shlex.quote(c) for c in err.cmd])
-        lines = err.stderr.strip().split('\n')
-        if len(lines) > 40:
-            lines = lines[:40]
-        for l in lines:
-            print(l)
-        print("!!!Build ERROR!!!")
-        exit(1)
+    if args.clean:
+        build_outputs = {
+            '.o', '.o++', ''
+        }
+        env.deps(targ).value()
+        for path in env.files:
+            if path.exists() and path.suffix.lower() in build_outputs:
+                path.unlink()
+    else:
+        try:
+            env.build(targ).value()
+        except sp.CalledProcessError as err:
+            print(*[shlex.quote(c) for c in err.cmd])
+            lines = err.stderr.strip().split('\n')
+            if len(lines) > 40:
+                lines = lines[:40]
+            for l in lines:
+                print(l)
+            print("!!!Build ERROR!!!")
+            exit(1)
